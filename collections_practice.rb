2fb6487248904025ad6d8b7f92e1a97dda98496b
Scott Jordan <scottjordanswimming@gmail.com>
2019-09-23 12:05:21 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -53,6 +53,6 @@ end
 
 def add_s(array)
   
-  array.each.collect {|word| word << "s" if word != "feet" else "feet"}
+  array.each.collect {|word| word << "s" if word != "feet" else word = "feet"}
   
 end
